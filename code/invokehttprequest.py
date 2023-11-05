c149258ab667cbc5954d4e5a22d175ff2ff6d5d8
parama siva <addala.paramasiva55@gmail.com>
2023-11-05 19:26:23 +0530

    updated py

--- code/invokehttprequest.py
+++ code/invokehttprequest.py
@@ -1,12 +1,11 @@
-import http.client,urllib.parse
+import http.client
 import os
 
 def lambda_handler(event,context):
     subnet_id = os.environ.get("subnet_id")
     conn = http.client.HTTPSConnection('ij92qpvpma.execute-api.eu-west-1.amazonaws.com/')
 
-    params = urllib.parse.urlencode({ "@event": "context"})
-    conn.request("POST","/candidate-email_serverless_lambda_stage/data",params,headers={'X-Siemens-Auth': 'test'})
+    conn.request("POST","/candidate-email_serverless_lambda_stage/data",headers={'X-Siemens-Auth': 'test'})
     
     response = conn.getresponse()
     print(response.read().decode())
--- sample.py
+++ sample.py
@@ -3,7 +3,7 @@ host = "ij92qpvpma.execute-api.eu-west-1.amazonaws.com"
 conn = http.client.HTTPSConnection(host)
 
 payload = urllib.parse.urlencode({
-  "subnet_id": "<Your Private Subnet ID>"
+  "subnet_id": "subnet1"
 })
 conn.request("POST", "/candidate-email_serverless_lambda_stage/data",payload, headers={'X-Siemens-Auth': 'test'})
 response = conn.getresponse()
