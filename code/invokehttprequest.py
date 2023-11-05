59fe77219f1ee57768adf8942308ae76457cd4d6
parama siva <addala.paramasiva55@gmail.com>
2023-11-05 22:44:02 +0530

    updated py

--- code/invokehttprequest.py
+++ code/invokehttprequest.py
@@ -2,10 +2,10 @@ import http.client
 import os
 
 def lambda_handler(event,context):
-    subnet_id = os.environ.get("subnet_id")
+    # subnet_id = os.environ.get("subnet_id")
     conn = http.client.HTTPSConnection('ij92qpvpma.execute-api.eu-west-1.amazonaws.com')
     payload = {
-    "subnet_id": "${subnet_id}",
+    "subnet_id": os.environ.get("subnet_id"),
     "name": "addala Paramasiva",
     "email": "addala.paramasiva55@gmail.com"
     }
