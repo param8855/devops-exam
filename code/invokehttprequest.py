ef585937eb139d9b687c6901f3c69c544662955e
parama siva <addala.paramasiva55@gmail.com>
2023-11-05 23:40:55 +0530

    updated py

--- code/invokehttprequest.py
+++ code/invokehttprequest.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import json
+import urllib3
 def handler(event, context):
     url = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
     headers = {
