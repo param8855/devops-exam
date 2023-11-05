9abc9666dc0f083fabb028ccff67e53d923e9b43
parama siva <addala.paramasiva55@gmail.com>
2023-11-05 23:42:16 +0530

    updated py

--- code/invokehttprequest.py
+++ code/invokehttprequest.py
@@ -2,7 +2,7 @@ import os
 import requests
 import json
 import urllib3
-def handler(event, context):
+def lambda_handler(event, context):
     url = "https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
     headers = {
         "X-Siemens-Auth": "test"
@@ -15,4 +15,5 @@ def handler(event, context):
 
     response = requests.post(url, headers=headers, json=payload)
     return response.json()
+
 
