d36274fbc91ef0a38b304e196c950f2db007ea02
parama siva <addala.paramasiva55@gmail.com>
2023-11-05 19:03:42 +0530

    update jenkin file

--- code/invokehttprequest.py
+++ code/invokehttprequest.py
@@ -1,13 +1,11 @@
-import http.client,urllib.parse
+import http.client
 import os
 
-def lambda_handler():
+def lambda_handler(event,context):
     subnet_id = os.environ.get("subnet_id")
     conn = http.client.HTTPSConnection('ij92qpvpma.execute-api.eu-west-1.amazonaws.com/')
-    
-    payload = urllib.parse.urlencode({ "subnet_id": "subnet-1a","name": "addala paramasiva","email": "addala.paramasiva55@gmail.com"})
-    
-    conn.request("POST","/candidate-email_serverless_lambda_stage/data", payload,headers={'X-Siemens-Auth': 'test'})
+    params = {event,context}    
+    conn.request("POST","/candidate-email_serverless_lambda_stage/data",params,headers={'X-Siemens-Auth': 'test'})
     
     response = conn.getresponse()
     print(response.read().decode())
---
+++ sample.py
@@ -0,0 +1,12 @@
+import http.client,urllib.parse
+host = "ij92qpvpma.execute-api.eu-west-1.amazonaws.com"
+conn = http.client.HTTPSConnection(host)
+
+payload = urllib.parse.urlencode({
+  "subnet_id": "<Your Private Subnet ID>",
+  "name": "<Your Full Name>",
+  "email": "<Your Email Address>"
+})
+conn.request("POST", "/candidate-email_serverless_lambda_stage/data",payload, headers={'X-Siemens-Auth': 'test'})
+response = conn.getresponse()
+print(response.status, response.reason)
