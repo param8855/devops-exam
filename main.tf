fc225ac79504fa0af1e20b5b6120ee7648e584ed
parama siva <addala.paramasiva55@gmail.com>
2023-11-05 17:42:31 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -61,12 +61,12 @@ resource "aws_security_group" "lambda_sg" {
   name        = "lambda-sg"
   vpc_id      = data.aws_vpc.vpc.id
 
-  ingress {
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["10.0.0.0/16"]
-  }
+#   ingress {
+#     from_port        = 443
+#     to_port          = 443
+#     protocol         = "tcp"
+#     cidr_blocks      = ["10.0.0.0/16"]
+#   }
 
   egress {
     from_port        = 0
