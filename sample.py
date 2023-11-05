3c18511f2f3a3dca9357b04c596567219e5f90fd
parama siva <addala.paramasiva55@gmail.com>
2023-11-05 19:15:54 +0530

    updated py

--- sample.py
+++ sample.py
@@ -3,10 +3,9 @@ host = "ij92qpvpma.execute-api.eu-west-1.amazonaws.com"
 conn = http.client.HTTPSConnection(host)
 
 payload = urllib.parse.urlencode({
-  "subnet_id": "<Your Private Subnet ID>",
-  "name": "<Your Full Name>",
-  "email": "<Your Email Address>"
+  "subnet_id": "<Your Private Subnet ID>"
 })
 conn.request("POST", "/candidate-email_serverless_lambda_stage/data",payload, headers={'X-Siemens-Auth': 'test'})
 response = conn.getresponse()
 print(response.status, response.reason)
+
